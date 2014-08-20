tizenorg/profile.ivi.message-port

Fix TIVI-2638 Enable SmackExecLabel=User for messageport
Support for invoking messageport daemon by systemd with correct smack label.

This commit is also having the supported changes :
 - Stop using sessin bus for invoking and fix tests in non-session-bus case.
 - change in daemon socket location : /run/user/@usern_name@ -> /tmp.
   As user specific folder is not created at the time of daemon startup

Change-Id: Ibd885e522970aa63241792258e4a32a0bf32cfbb

tests/test-app.c
@@ -400,6 +400,7 @@ _on_term (gpointer userdata)
 static gboolean
 test_setup ()
 {
+#ifdef USE_SESION_BUS
     GIOChannel *channel = NULL;
     gchar *bus_address = NULL;
     gint tmp_fd = 0;
@@ -445,6 +446,7 @@ test_setup ()
     g_print ("Dbus daemon start at : %s\n", bus_address);
 
     g_free (bus_address);
+#endif /* USE_SESSION_BUS */
 
     return TRUE;
 }
@@ -452,7 +454,9 @@ test_setup ()
 static void
 test_cleanup ()
 {
+#ifdef USE_SESION_BUS
    if (__daemon_pid) kill (__daemon_pid, SIGTERM);
+#endif /* USE_SESSION_BUS */
 }
 
 

tizenorg/platform.core.appfw.message-port-dbus

 - Stop using session bus for invoking, and fix tests in non-session-bus case.

tests/test-app.c
@@ -400,6 +400,7 @@ _on_term (gpointer userdata)
 static gboolean
 test_setup ()
 {
+#ifdef USE_SESSION_BUS
     GIOChannel *channel = NULL;
     gchar *bus_address = NULL;
     gint tmp_fd = 0;
@@ -445,6 +446,7 @@ test_setup ()
     g_print ("Dbus daemon start at : %s\n", bus_address);
 
     g_free (bus_address);
+#endif /* USE_SESSION_BUS */
 
     return TRUE;
 }
@@ -452,7 +454,9 @@ test_setup ()
 static void
 test_cleanup ()
 {
+#ifdef USE_SESSION_BUS
    if (__daemon_pid) kill (__daemon_pid, SIGTERM);
+#endif /* USE_SESSION_BUS */
 }
 
 